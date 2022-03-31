6102781a8a9efa73d70562fe5e9eeae044379524
Nowak20p <91263517+Nowak20p@users.noreply.github.com>
2022-03-31 09:35:08 +0200

    Add files via upload

---
+++ rejestracja.php
@@ -0,0 +1,11 @@
+<?php
+
+$nazwa = $_POST['nazwa'];
+$haslo = $_POST['nazwa'];
+$emai = $_POST['email'];
+$miasto = $_POST['miasto'];
+$kod = $_POST['kod'];
+$ulica = $_POST['ulica'];
+$nrDomu = $_POST['nrDomu'];
+$check = $_POST['check'];
+$submit = $_POST['submit'];
