c468296e0f5fcd929905a2b0f242e14e0b32623b
Nowak20p <91263517+Nowak20p@users.noreply.github.com>
2022-04-21 11:11:43 +0200

    małe zmiany

--- rejestracja.php
+++ rejestracja.php
@@ -20,33 +20,20 @@ $kodVal = '/^[0-9]{2}-[0-9]{3}$/';
 $ulicaVal = '/^[A-Za-zĄĘĆŁŃÓŚŻŹąęćłńóśżź]{3,100}$|^[A-Za-zĄĘĆŁŃÓŚŻŹąęćłńóśżź]+[A-Za-zĄĘĆŁŃÓŚŻŹąęćłńóśżź0-9\s\-\.\/]+[A-Za-zĄĘĆŁŃÓŚŻŹąęćłńóśżź0-9]{1,100}$/';
 
 
-if(preg_match($nazwaVal,$nazwa)||(preg_match($hasloVal,$haslo)||(preg_match($emailVal,$email)||(preg_match($miastoVal,$miasto)||(preg_match($miastoVal,$miasto)
+if (preg_match($nazwaVal, $nazwa) || (preg_match($hasloVal, $haslo)) || (preg_match($emailVal, $email)) || (preg_match($miastoVal, $miasto)) || (preg_match($miastoVal, $miasto)) || (preg_match($kodVal, $kod)) || (preg_match($ulicaVal, $ulica))) {
+
+    $tablica = array("nazwa" => $nazwa, "haslo" => $haslo, "email" => $email, "miast" => $miasto, "kod" => $kod, "ulica" => $ulica, "nrDomu" => $nrDomu); // tworze sobie tablice z wszytkim
+
+    $fileJson = file_get_contents("user.json"); // bierze zawartosc tablicy
+    $tablica2 = json_decode($fileJson); // zmiena stringa na json
+    array_push($tablica2->users, $tablica); // dodanie tablicy z danymi do users
+    // echo var_dump($tablica2);
+    $tablica2 = json_encode($tablica2); // zamiana jsona na streinga 
 
 
+    file_put_contents("user.json", $tablica2); //dodanie danych do pliku z users
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$tablica = array("nazwa" => $nazwa, "haslo" => $haslo, "email" => $email, "miast" => $miasto, "kod" => $kod, "ulica" => $ulica, "nrDomu" => $nrDomu); // tworze sobie tablice z wszytkim
-
-$fileJson = file_get_contents("user.json"); // bierze zawartosc tablicy
-$tablica2 = json_decode($fileJson); // zmiena stringa na json
-array_push($tablica2->users, $tablica); // dodanie tablicy z danymi do users
-// echo var_dump($tablica2);
-$tablica2 = json_encode($tablica2); // zamiana jsona na streinga 
-
-
-file_put_contents("user.json", $tablica2); //dodanie danych do pliku z users
+    echo "Dziekujemy za utworzenie konta";
+} else {
+    echo "Nieprprawnie wpisane dane";
+}
